964c61f99fd3d1d647d868d8b2a32ae8a9bc01a0
yeggrrr <yjkim981103@gmail.com>
2024-07-22 18:17:53 +0900

    [Feat] Observable 클래스 추가

---
+++ YegrPolaroidEX/Observable/Observable.swift
@@ -0,0 +1,26 @@
+//
+//  Observable.swift
+//  YegrPolaroidEX
+//
+//  Created by YJ on 7/22/24.
+//
+
+import Foundation
+
+class Observable<T> {
+    var closure: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            closure?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(closure: @escaping (T) -> Void) {
+        self.closure = closure
+    }
+}
