a288a2a92ad37436075d0c7e0aff5a4653bc22b7
yeggrrr <yjkim981103@gmail.com>
2024-07-22 18:25:47 +0900

    [Feat] UserDefaultManager 생성

---
+++ YegrPolaroidEX/UserDefaults/UserDefaultsManager.swift
@@ -0,0 +1,34 @@
+//
+//  UserDefaultsManager.swift
+//  YegrPolaroidEX
+//
+//  Created by YJ on 7/22/24.
+//
+
+import Foundation
+
+enum UserDefaultsUserInfo: String {
+    case isExistUser
+    case name
+    case profileImage
+}
+
+struct UserDefaultsManager {    
+    static var userTempProfileImageName: String?
+    
+    static func fetchisExistUser() -> Bool {
+        return UserDefaults.standard.bool(forKey: UserDefaultsUserInfo.isExistUser.rawValue)
+    }
+    
+    static func save(value: Any, key: UserDefaultsUserInfo) {
+        UserDefaults.standard.setValue(value, forKey: key.rawValue)
+    }
+    
+    static func fetchName() -> String {
+        return UserDefaults.standard.string(forKey: UserDefaultsUserInfo.name.rawValue) ?? "-"
+    }
+    
+    static func fetchProfileImage() -> String {
+        return UserDefaults.standard.string(forKey: UserDefaultsUserInfo.profileImage.rawValue) ?? "-"
+    }
+}
