0163c8d7bf1449ac46fb171d567e9f2708ca6457
yeggrrr <yjkim981103@gmail.com>
2024-07-26 14:57:27 +0900

    [Fix] DateFormatter 수정

--- YegrPolaroidEX/Sources/Extension/DateFormatter+.swift
+++ YegrPolaroidEX/Sources/Extension/DateFormatter+.swift
@@ -14,4 +14,16 @@ extension DateFormatter {
         dateFormat.dateFormat = "yyyy년 MM월 dd일"
         return dateFormat
     }()
+    
+    static let stringToDateFormatter: ISO8601DateFormatter = {
+        let dateFormat = ISO8601DateFormatter()
+        return dateFormat
+    }()
+    
+    static func dateToContainLetter(dateString: String) -> String {
+        guard let date = DateFormatter.stringToDateFormatter.date(from: dateString) else { return "-" }
+        return DateFormatter.containLettersDateFormatter.string(from: date)
+    }
 }
+
+
