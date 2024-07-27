170d4c7f2ee0d176556d38f3c4bedcd36b7831a0
yeggrrr <yjkim981103@gmail.com>
2024-07-28 04:13:48 +0900

    [Feat] UIButton Extension 추가 - 이미지 상태에 따른 배경색상 변경 메서드

--- YegrPolaroidEX/Sources/Extension/UIButton+.swift
+++ YegrPolaroidEX/Sources/Extension/UIButton+.swift
@@ -34,3 +34,20 @@ extension UIButton {
         clipsToBounds = true
     }
 }
+
+extension UIButton {
+    // 이미지 상태별 배경색상 변경
+    func setBackgroundColor(color: UIColor, forState: UIControl.State) {
+        self.clipsToBounds = true
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+        
+        if let context = UIGraphicsGetCurrentContext() {
+            context.setFillColor(color.cgColor)
+            context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+            
+            let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            self.setBackgroundImage(colorImage, for: forState)
+        }
+    }
+}
